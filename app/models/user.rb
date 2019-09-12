83336809fabcbdfded1cc8ef6e92083d1a02c08f
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-09-13 01:49:27 +0900

    Add validation for User model(uniqueness) and email is downcased before save.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
   validates(:name, presence: true, length: { maximum: 20 })
   REGEX_FOR_VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates(:email, presence: true, length: { maximum: 128 }, format: { with: REGEX_FOR_VALID_EMAIL })
+  validates(:email, presence: true, length: { maximum: 128 }, uniqueness: { case_sensitive: false }, format: { with: REGEX_FOR_VALID_EMAIL })
 end
