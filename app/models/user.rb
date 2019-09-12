6889ba49f370e52c9d00cad0b2cc7e6d07aecc19
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-09-13 01:44:15 +0900

    Add validation for User model(regex for valid email)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates(:name, presence: true, length: { maximum: 20 })
-  validates(:email, presence: true, length: { maximum: 128 })
+  REGEX_FOR_VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates(:email, presence: true, length: { maximum: 128 }, format: { with: REGEX_FOR_VALID_EMAIL })
 end
