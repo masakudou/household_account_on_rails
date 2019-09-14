fbc8e1e2365f34d49ce824f294d4e7a901ccfefb
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-09-14 20:54:21 +0900

    Add 'img' and 'header_image' column.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_12_165326) do
+ActiveRecord::Schema.define(version: 2019_09_14_115139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_09_12_165326) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "img"
+    t.string "header_image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
