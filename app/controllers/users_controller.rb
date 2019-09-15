899f160d249be128d9c53fe06827950dbefb21cf
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-09-15 23:32:26 +0900

    Add create action for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.img = "default_img.png"
+    @user.header_image = "default_header_image.png"
+    if @user.save
+      flash[:success] = "ようこそ！ #{@user.name}さん！"
+      redirect_to @user
+    else
+      render('new')
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
