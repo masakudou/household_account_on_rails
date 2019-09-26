f8669d2a17c3ba0020dc03b04e0f828331dc7775
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-09-26 22:15:21 +0900

    Set validation for BookRecord.

--- app/models/book_record.rb
+++ app/models/book_record.rb
@@ -1,3 +1,8 @@
 class BookRecord < ApplicationRecord
   belongs_to :user
+  validates(:user_id, presence: true)
+  validates(:type, presence: true)
+  validates(:category, presence: true)
+  validates(:amount, presence: true)
+  validates(:record_date, presence: true)
 end
