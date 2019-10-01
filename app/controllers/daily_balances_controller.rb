26484acddc6e7bda258711dfa6c576547d9a5b4e
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-10-02 01:24:43 +0900

    Fixed 'daily_balances.show' action.

--- app/controllers/daily_balances_controller.rb
+++ app/controllers/daily_balances_controller.rb
@@ -1,7 +1,7 @@
 class DailyBalancesController < ApplicationController
   def show
     @user = User.find_by(id: params[:user_id])
-    @daily_balances = current_user.daily_balances.find_by(id: @user.id)
-    @book_records = current_user.book_records.where(record_date: @daily_balances.record_date)
+    @daily_balances = @user.daily_balances.find_by(id: params[:id])
+    @book_records = @user.book_records.where(record_date: @daily_balances.record_date)
   end
 end
