6ef7e22d821daeaeae6b169efb61ec38a0e7f7a8
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-10-01 22:42:46 +0900

    Added a link to the view that shows the daily balance on the calendar.

--- app/controllers/daily_balances_controller.rb
+++ app/controllers/daily_balances_controller.rb
@@ -1,6 +1,6 @@
 class DailyBalancesController < ApplicationController
   def show
-    @daily_balances = current_user.daily_balances.find_by(record_date: params[:record_date])
-    @book_records = current_user.book_records.where(record_date: params[:record_date])
+    @daily_balances = current_user.daily_balances.find_by(id: params[:id])
+    @book_records = current_user.book_records.where(record_date: @daily_balances.record_date)
   end
 end
