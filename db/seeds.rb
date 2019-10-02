ffc75d0c3700b405b4288215bd80b70645e13507
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-10-03 02:25:15 +0900

    Add category initialized record to seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 User.create!(
   name: "mintia96",
   email: "wazap1121@gmail.com",
@@ -12,3 +14,22 @@ User.create!(
   password_confirmation: "12345678"
 )
 
+Category.create!(
+  name: "食費",
+  color: "#ff8ab5"
+)
+
+Category.create!(
+  name: "交通費",
+  color: "#8ac1ff"
+)
+
+Category.create!(
+  name: "趣味",
+  color: "#8affbb"
+)
+
+Category.create!(
+  name: "その他",
+  color: "#d4d4d4"
+)
