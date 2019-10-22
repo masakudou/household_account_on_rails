dfb81266671617a2c3fe383c59bcdf898f5eae10
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-10-22 19:23:03 +0900

    Solves issue #61.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,33 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  # Categoryモデルのレコード分+未定義分のパラメーター配列を初期化する。
+  def initialize_parameter_array
+    all_category = Category.all
+    parameter_array = []
+    all_category.each do |category|
+      parameter_array.push([category.name, 0, category.color])
+    end
+
+    parameter_array.push(["未分類", 0, "#BBBBBB"])
+  end
+
+  # parameterの第3引数(色コード)を別の配列に分離する
+  def separate_color_column(parameters)
+    colors = []
+    parameters.each do |parameter|
+      colors.push(parameter[2])
+      parameter.delete_at(-1)
+    end
+
+    return parameters, colors
+  end
+
+  # 支出額が0であるカテゴリのパラメーターは削除する
+  def delete_if_amount_is_zero(parameters)
+    parameters.delete_if do |parameter|
+      parameter[1].zero?
+    end
+  end
 end
