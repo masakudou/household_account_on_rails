ccadd05024978a6e546c1f1a14578a95c983b189
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-10-23 01:10:08 +0900

    set false to 'config.assets.initialize_on_precompile on application.rb.'

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module HouseholdAccountOnRails
     config.beginning_of_week = :sunday
     # タイムゾーン
     config.time_zone = 'Tokyo'
+    # 部分的にアプリケーションを読み込む。Herokuデプロイ時に必要。
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
