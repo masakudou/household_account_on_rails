c986b40e1ae62669838650a9cf1f720e6501e8bd
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-10-28 19:34:21 +0900

    Moved the daily_balances_helper function to the BookRecord model member function to make it behave like a member variable.

--- app/models/book_record.rb
+++ app/models/book_record.rb
@@ -6,4 +6,16 @@ class BookRecord < ApplicationRecord
   validates(:amount, presence: true, numericality: { greater_than: 0 })
   validates(:record_date, presence: true)
   validates(:comment, length: { maximum: 140 })
+
+  # 実際にはメンバ変数ではないが、メンバ変数のように振舞うメソッド
+  # ex. book_record.category_name => 趣味
+  def category_name
+    category = Category.find_by(id: self.category)
+    category.nil? ? "未分類" : category.name
+  end
+
+  def category_color
+    category = Category.find_by(id: self.category)
+    category.nil? ? "#BBBBBB" : category.color
+  end
 end
