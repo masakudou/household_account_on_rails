4661269dc2cf8a7b334f00babd18dcd13eaa4cca
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-11-06 22:52:00 +0900

    Create scenario 'Change password' on change_user_settings feature spec test.

--- spec/features/change_user_settings_spec.rb
+++ spec/features/change_user_settings_spec.rb
@@ -46,4 +46,30 @@ RSpec.feature "ChangeUserSettings", type: :feature do
     expect(current_path).to eq root_path
     expect(page).to have_content "#{@user.name} さんの収支カレンダー"
   end
+
+  scenario "Change password" do
+    # ログイン処理
+    log_in(@user)
+    # 設定をクリック
+    click_on "設定"
+    # フォームに入力
+    fill_in "user_password", with: "aabbccdd"
+    fill_in "user_password_confirmation", with: "aabbccdd"
+    # 設定の変更
+    click_button "設定の変更"
+    # フラッシュメッセージを確認する
+    expect(page).to have_content "基本情報を更新しました。"
+    # 一度ログアウトする
+    click_on "ログアウト"
+    # ログインを試みる
+    log_in(@user)
+    # エラーメッセージが表示されることを確認する
+    expect(page).to have_content "メールアドレスもしくはパスワードが違います。"
+    # インスタンス変数のpasswordを変更後の文字列に置き換える
+    @user.password = "aabbccdd"
+    # もう一度ログインし、今度は正常にログインできることを確認する
+    log_in(@user)
+    expect(current_path).to eq root_path
+    expect(page).to have_content "#{@user.name} さんの収支カレンダー"
+  end
 end
