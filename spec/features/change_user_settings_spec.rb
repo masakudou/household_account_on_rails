3cb523d252c175ebb50f5c33a92d201ebee28171
Ryunosuke Yamada <chihaya9373-adlt@yahoo.co.jp>
2019-11-06 22:49:56 +0900

    Create scenario 'Change email' on change_user_settings feature spec test.

--- spec/features/change_user_settings_spec.rb
+++ spec/features/change_user_settings_spec.rb
@@ -21,4 +21,29 @@ RSpec.feature "ChangeUserSettings", type: :feature do
     visit root_path
     expect(page).to have_content "rails user さんの収支カレンダー"
   end
+
+  scenario "Change email" do
+    # ログイン処理
+    log_in(@user)
+    # 設定をクリック
+    click_on "設定"
+    # フォームに入力
+    fill_in "user_email", with: "changed@sample.com"
+    # 設定の変更
+    click_button "設定の変更"
+    # フラッシュメッセージを確認する
+    expect(page).to have_content "基本情報を更新しました。"
+    # 一度ログアウトする
+    click_on "ログアウト"
+    # ログインを試みる
+    log_in(@user)
+    # エラーメッセージが表示されることを確認する
+    expect(page).to have_content "メールアドレスもしくはパスワードが違います。"
+    # インスタンス変数のemailを変更後の文字列に置き換える
+    @user.email = "changed@sample.com"
+    # もう一度ログインし、今度は正常にログインできることを確認する
+    log_in(@user)
+    expect(current_path).to eq root_path
+    expect(page).to have_content "#{@user.name} さんの収支カレンダー"
+  end
 end
